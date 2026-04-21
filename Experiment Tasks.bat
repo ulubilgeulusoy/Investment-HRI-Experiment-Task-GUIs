f70c9b75b887185a3cc805c0aab706d58cc046f9
Ulubilge Ulusoy <ulubilgeulusoy@gmail.com>
2026-04-21 16:10:05 -0600

    desktop launch file added

--- Experiment Tasks.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Launch combined_experiment_GUI.py using conda env: computer_vision
-pushd "%~dp0"
-call "C:\Users\investment\miniconda3\Scripts\activate.bat"
-call conda activate computer_vision
-python "combined_experiment_GUI.py"
-popd
